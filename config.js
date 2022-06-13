0a84b099279c71f4e94a65a8845f5bfab19b8875
Sahrull11 <107224311+Sahrull11@users.noreply.github.com>
2022-06-13 18:25:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.web = 'https://youtu.be/jOhhY7ef_qM' //ubah jadi website lu, bisa link ig
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/hiruko.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/hiruko.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ArullBotzMD'
