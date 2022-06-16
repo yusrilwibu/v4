2e34c61d7a1621e02ab27cf7b2f9d3aff0bff754
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-16 22:19:12 +0800

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,15 +1,15 @@
 
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-  if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  if (!m.quoted) throw `*balas stiker dengan caption* *${usedPrefix + command}*`
   let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  if (!/webp/.test(mime)) throw `*balas stiker dengan caption* *${usedPrefix + command}*`
   let media = await m.quoted.download()
   let out = Buffer.alloc(0)
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
+  await conn.sendFile(m.chat, out, 'out.png', '*NIHH*', m, false, {
     thumbnail: Buffer.alloc(0)
   })
 }
