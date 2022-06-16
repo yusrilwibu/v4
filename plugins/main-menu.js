5e29f850f128c9b884260085a20b29eefd41bdb8
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-16 23:03:10 +0800

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -283,12 +283,12 @@ const fdoc = {
 
 //━━━━━━━━[ BAGIAN MENU ]━━━━━━━━//
 if (teks == '404') {
-let menuu = `'┌────「 *${namebot}* 」───⬣
+let menuu = `┌────「 *${namebot}* 」───⬣
 │⬡ Aktif selama ${uptime}
 │⬡ ${Object.keys(global.db.data.users).length} Pengguna
 │⬡ Mode : ${global.opts['self'] ? 'Self' : 'publik'}
 │⬡ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} Chat Terbanned
-│⬡ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} Pengguna Terbanned', 
+│⬡ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} Pengguna Terbanned
 ⮕ ${groupsIn.length} - Group Chats
 ⮕ ${groupsIn.length} - Groups Joined
 ⮕ ${groupsIn.length - groupsIn.length} - Groups Left
