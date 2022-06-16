97e3e98a9bcd87bb321858abbddb8f84342616d4
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-16 16:15:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.web = 'https://youtu.be/jOhhY7ef_qM' //ubah jadi website lu, bisa link ig
 global.price1 = 'Permanen 15.000\n1 bulan 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/hiruko.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/hiruko.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/d60f1b8e902b3f43d8701.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/00009d2ade51e015153ad.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ArullBotzMD\nJangan Spam Ya Kontoll'
