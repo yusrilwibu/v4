d5420eed4a9be5875390d4e63d1cc8e382b64b97
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-16 14:38:09 +0800

    Update main-waalaikummussalam.js

--- plugins/main-waalaikummussalam.js
+++ plugins/main-waalaikummussalam.js
@@ -1,5 +1,5 @@
 let handler = async (m, {conn}) => {
-let anu = '\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n\n_waalaikumussalam wr.wb._'
+let anu = '\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n\n_waalaikumussalam wr.wb._\n\n🍂Menjawab salam adalah wajib. Apabila ada seseorang melewati sekumpulan kaum Muslimin, kemudian dia memberi salam kepada mereka, namun mereka tidak mau menjawabnya, maka ruh al-quds (ruh yang suci) akan dicabut dari diri mereka, dan yang menjawab salam orang tersebut adalah malaikat'
 m.reply(anu)  
 }
 handler.customPrefix = /^ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم$/i // ketik bot (tanpa prefix)
