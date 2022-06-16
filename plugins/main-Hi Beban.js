b97cc952918f8306cab99748a858856a98db7e75
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-16 17:39:20 +0800

    Create main-Hi Beban.js

---
+++ plugins/main-Hi Beban.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let name = await conn.getName(m.sender)
+  let wm = global.wm
+hyzer = {
+    key : {
+  remoteJid: 'status@broadcast',
+  participant : '0@s.whatsapp.net'
+    },
+  message: { 
+  "extendedTextMessage": {
+  "text": '*Sama-Sama Kak*',
+  "title": wm,
+  'jpegThumbnail': 'https://telegra.ph/file/20509b31b6e05ff999daa.jpg',
+        }
+       } 
+     }
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/4c97e4a74649720aaa120.jpg', hyzer, { packname: "sticker by", author: "ArullOfc" })
+}
+
+handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
+handler.command = new RegExp
+
+module.exports = handler
