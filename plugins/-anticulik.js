4c754e2ef86a082e36f085b497d252234fa4ff2a
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-17 07:34:05 +0800

    Update -anticulik.js

--- plugins/-anticulik.js
+++ plugins/-anticulik.js
@@ -2,16 +2,16 @@ let handler = m => handler.all = async function (m, { isAdmin, isBotAdmin }) {
   if (m.mtype == 'groupInviteMessage') {
 idnya = await this.reply(m.chat, `「 Donasi • Pulsa 」
 
-• *Gopay:* 6281215199447
-• *Telkomsel:* 6281215199447
-• *Indosat:* 6285642337380
+• *Gopay:* 
+• *Axis:* 6283843192208
+• *Indosat:* 6285822347348
 
 Donasi semampunya untuk mendukung bot ini tetap hidup untuk kalian^_^
-Kalau gak mampu yang di atas bisa juga *follow ig* https://instagram.com/caliph91_
+Kalau gak mampu yang di atas bisa juga *follow ig* https://instagram.com/sahrulwara_____
 
 Kontak pemilik saya⬇️⬇️
 `, m)
-this.sendContact(m.chat, '6281215199447', 'Pemilik saya', idnya)
+this.sendContact(m.chat, '6285822347348', 'Pemilik saya', idnya)
   }
   return true
 }
