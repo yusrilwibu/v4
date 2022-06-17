fc915f42595a4b3492d0f64349aa08da998ff188
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-17 10:26:04 +0800

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,20 +1,13 @@
 let fetch = require('node-fetch')
-
-let handler = m => m
-
-handler.before = async (m) => {
-    let chat = db.data.chats[m.chat]
-    if (chat.simi && !chat.isBanned) {
-        if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
-        if (!m.text) return
-        let res = await fetch(API('xteam', '/simsimi', { kata: encodeURIComponent(m.text) }, 'APIKEY'))
-        if (!res.ok) return m.reply(`${res.status} ${res.statusText}`)
-        let json = await res.json()
-        if (!json.status) return m.reply(require('util').format(json))
-        m.reply('json.jawaban')
-        return !0
-    }
-    return !0
+let handler = async (m, { text }) => {
+  let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (json.status) m.reply(json.result)
+  else throw json
 }
+handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+handler.tags = ['fun']
+handler.command = /^((sim)?simi|simih)$/i
 
 module.exports = handler
