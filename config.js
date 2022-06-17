fbd29c453760eccc03a0001cf74003ea8d32abd6
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-17 09:49:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.price1 = 'Permanen 15.000\n1 bulan 10.000\n2 Minggu 7.000\n1 Minggu 5.000
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/aea4991822786add62469.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/00009d2ade51e015153ad.jpg' //ini juga ganti 
-global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.lolkey = 'Danwfrostkey' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ArullBotzMD\nJangan Spam Ya Kontoll'
 global.watermark = wm
