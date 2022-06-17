7720cb17c9429f66409e9f1bb6c930aea5bda938
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-17 15:26:27 +0800

    Update main-waalaikummussalam.js

--- plugins/main-waalaikummussalam.js
+++ plugins/main-waalaikummussalam.js
@@ -1,15 +1,19 @@
-let handler = async (m, { conn }) => {
-let caption = `*Waalaikummussalam warahmatullahi wabarokatuh*
-
-
-_📚 Baca yang dibawah ya!_
+let handler = async (m, {conn}) => {
+let anu = '\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n\n_waalaikumussalam wr.wb._\n_📚 Baca yang dibawah ya!_\n
 "Orang yang mengucapkan salam seperti ini maka ia mendapatkan 30 pahala, kemudian, orang yang dihadapan atau mendengarnya membalas dengan kalimat yang sama yaitu “Wa'alaikum salam warahmatullahi wabarakatuh” atau ditambah dengan yang lain (waridhwaana). Artinya selain daripada do'a selamat juga meminta pada Allah SWT"
-`
-
-conn.sendButton( m.chat, caption, `Salam`, 'Waalaikummussalam','warahmatullahi wabarokatuh', m)
-
-       }
-       
-handler.customPrefix = /^(assalamualaikum|Salom|salam)/i
+'
+m.reply(anu)  
+}
+handler.customPrefix = /^ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم$/i // ketik bot (tanpa prefix)
 handler.command = new RegExp
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
 module.exports = handler
+
+
