9bfa81122252b8bde7c4e23a5fd0530f25b4d60e
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-17 08:05:00 +0800

    Update owner-enable.js

--- plugins/owner-enable.js
+++ plugins/owner-enable.js
@@ -1,56 +1,39 @@
-llet handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
   let isEnable = /true|enable|(turn)?on|1/i.test(command)
-  let chat = global.DATABASE._data.chats[m.chat]
-  let user = global.DATABASE._data.users[m.sender]
+  let chat = global.db.data.chats[m.chat]
+  let user = global.db.data.users[m.sender]
+  let setting = global.db.data.settings
   let type = (args[0] || '').toLowerCase()
   let isAll = false
   let isUser = false
   switch (type) {
+    case 'w':
     case 'welcome':
-      chat.welcome = isEnable
-      break
-    case 'detect':
       if (!m.isGroup) {
         if (!isOwner) {
           global.dfail('group', m, conn)
           throw false
         }
-      } else if (!isAdmin) {
+      } else if (!(isAdmin || isOwner)) {
         global.dfail('admin', m, conn)
         throw false
       }
-      chat.detect = isEnable
+      chat.welcome = isEnable
       break
-    case 'delete':
-      if (m.isGroup) {
-        if (!isAdmin || !isOwner) {
-          global.dfail('admin', m, conn)
+    case 'antilink':
+    case 'antiurl':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn)
           throw false
         }
+      } else if (!(isAdmin || isOwner)) {
+        global.dfail('admin', m, conn)
+        throw false
       }
-      chat.delete = isEnable
+      chat.antiLink = isEnable
       break
-    case 'antidelete':
-      if (m.isGroup) {
-        if (!isAdmin || !isOwner) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.delete = !isEnable
-      break
-    case 'autodelvn':
-      if (m.isGroup) {
-        if (!isAdmin || !isOwner) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.autodelvn = isEnable
-      break
-    case 'document':
-      chat.useDocument = isEnable
-      break
+    case 'publik':
     case 'public':
       isAll = true
       if (!isROwner) {
@@ -59,41 +42,8 @@ llet handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, is
       }
       global.opts['self'] = !isEnable
       break
-      case 'restrict':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['restrict'] = isEnable
-      break
-      case 'nyimak':
-      isAll = true
-      if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['nyimak'] = isEnable
-      break
-    case 'antilink':
-      if (m.isGroup) {
-        if (!isAdmin || !isOwner) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.antiLink = isEnable
-      break
-          case 'antitoxic':
-    case 'toxic':
-          if (m.isGroup) {
-              if (!(isAdmin || isOwner)) {
-                  global.dfail('admin', m, conn)
-              }
-          }
-         chat.antiToxic = isEnable
-         break
     case 'autolevelup':
+    case 'levelup':
       isUser = true
       user.autolevelup = isEnable
       break
@@ -109,24 +59,90 @@ llet handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, is
       }
       conn.callWhitelistMode = isEnable
       break
-    case 'simi':
-      chat.simi = isEnable
+    case 'grup':
+    case 'gruponly':
+    case 'grouponly':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      setting.groupOnly = isEnable
+      break
+    case 'backup':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      setting.backup = isEnable
+      break
+    case 'anticall':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      setting.anticall = isEnable
+      break
+    case 'antitroli':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      setting.antitroli = isEnable
+      break
+    case 'autoread':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      opts['autoread'] = isEnable
+      break
+    case 'restrict':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      opts['restrict'] = isEnable
+      break
+    case 'nsfw':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      setting.nsfw = isEnable
+      break
+    case 'jadibot':
+      isAll = true
+      if (!isOwner) {
+        global.dfail('owner', m, conn)
+        throw false
+      }
+      setting.jadibot = isEnable
       break
     default:
       if (!/[01]/.test(command)) throw `
-List option: welcome | delete | public | antilink | autolevelup | antitoxic | detect | nyimak | restrict | document | whitelistmycontacts | simi
-
-Contoh:
-${usedPrefix}enable welcome
-${usedPrefix}disable welcome
+┌〔 Daftar Opsi 〕${isOwner ? '\n├ antispam\n├ antitroli\n├ autoread\n├ backup\n├ grouponly\n├ jadibot\n├ nsfw\n├ public\n├ antilink\n├ mycontact' : ''}
+├ autolevelup
+├ antilink
+├ welcome
+└────
+contoh:
+${usedPrefix}on welcome
+${usedPrefix}off welcome
 `.trim()
       throw false
   }
   m.reply(`
-*${type}* Berhasil Di *${isEnable ? 'nyala' : 'mati'}kan* ${isAll ? 'Untuk Bot Ini' : isUser ? '' : 'Untuk Chat Ini'}
+*${type}* berhasil di *${isEnable ? 'nyala' : 'mati'}kan* ${isAll ? 'untuk bot ini' : isUser ? '' : 'untuk chat ini'}
 `.trim())
 }
-handler.help = ['en', 'dis'].map(v => v + 'able <option>')
+handler.help = ['on', 'off'].map(v => v + ' <opsi>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
 
