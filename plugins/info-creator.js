fc1d5e6c5d5a624fd887d681875f9ee9fcd67208
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-19 12:05:18 +0800

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,10 +1,8 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+let handler = async(m, { conn }) => {
+conn.sendContact(m.chat, numberowner, nameowner, m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
 handler.command = /^(owner|creator)$/i
 
-export default handler
+module.exports = handler
