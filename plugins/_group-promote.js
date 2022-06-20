448172f4d8d199dc731300216d51f87c275ce201
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-20 12:26:17 +0800

    Update _group-promote.js

--- plugins/_group-promote.js
+++ plugins/_group-promote.js
@@ -8,9 +8,9 @@ let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
 if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
 let usr = m.quoted.sender;
 let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "promote"); return;
-if (nenen) m.reply(`sukses promote @${user.split('@')[0]}!`);
+if (nenen) m.reply(`*sukses promote* @${user.split('@')[0]}!`);
 }
-  if (!m.mentionedJid[0]) throw `siapa yang mau diadminin?`;
+  if (!m.mentionedJid[0]) throw `*siapa yang mau diadminin?*`;
   let users = m.mentionedJid.filter(
     (u) => !(u == ownerGroup || u.includes(conn.user.jid))
   );
