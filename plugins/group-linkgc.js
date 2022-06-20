7e55bc7b2598b9c53b8ea95bf6a49e15a8bdd353
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-20 22:12:55 +0800

    Update group-linkgc.js

--- plugins/group-linkgc.js
+++ plugins/group-linkgc.js
@@ -1,26 +1,15 @@
-let handler = async (m, { conn, args }) => {
-  let group = args[0] ? args[0] : m.chat
-  if (/^[0-9]{5,16}-[0-9]+@g\.us$/.test(args[0])) group = args[0]
-  if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
-  let groupMetadata = await conn.groupMetadata(group)
-  let groupName = m.isGroup ? groupMetadata.subject : ''
-  if (!groupMetadata) throw 'groupMetadata is undefined'
-  if (!'participants' in groupMetadata) throw 'participants is not defined'
-  let me = groupMetadata.participants.find(user => user.jid === conn.user.jid)
-  m.reply('*Link Group ini*\n\nhttps://chat.whatsapp.com/' + await conn.groupInviteCode(group))
+let handler = async (m, { conn }) => {
+  try {
+    conn.reply(m.chat, `*🤖Link Group:* ${await conn.getName(m.chat)}\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `\n\n${conn.user.name}`, m)
+  } catch {
+    conn.reply(m.chat, `*Jadikan* @${conn.user.jid.split('@')[0]} *sebagai admin untuk menggunakan perintah ini!*`, m, { mentions: [conn.user.jid] })
+  }
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
-handler.command = /^link(gro?up)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.command = /^link(g(c)?ro?up)?$/i
 
-handler.admin = false
-handler.botAdmin = true
-
-handler.fail = null
+handler.group = true
+//handler.botAdmin = true
 
 module.exports = handler
