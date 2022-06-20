f9a5dc004a18074dd7fbb2dad37bb630937c6510
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-20 21:44:20 +0800

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,16 +5,15 @@ let handler = async(m, { conn }) => {
 	ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
 	fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
 	conn.p[id] = [
-	await conn.sendContact(m.chat, numberowner, nameowner, { contexInfo: { forwardingScore: 99999, isForwarded: true } })
 	]
 	await delay(100)
-  return conn.sendMessage(m.chat, { text: `*Hallo kak* @${await m.sender.split('@')[0]}, *itu nomor ownerku,kalo ada kepentingan bisa dichat^_^*`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
+  return conn.sendMessage(m.chat, { text: `*Hallo kak* @${await m.sender.split('@')[0]}, *ada apa,kok manggil saya^_^*`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
   await delay(100)
   return delete conn.p[id]
 }
 
 handler.help = ['owner']
 handler.tags = ['info']
-handler.command = /^(owner|creator)$/i
+handler.command = /^(bot|)$/i
 
 module.exports = handler
