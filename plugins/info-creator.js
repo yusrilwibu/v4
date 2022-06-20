a3efbea3f86c982aa7934e11cd51128b45c6f4de
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-20 21:05:41 +0800

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -2,15 +2,10 @@ let handler = async(m, { conn }) => {
 conn.sendContact(m.chat, numberowner, nameowner, m)
 }
 await delay(100)
-
   return conn.sendMessage(m.chat, { text: `Hay kak @${await m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah ^_^`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
-
   await delay(100)
-
   return delete conn.p[id]
-
 }
-
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
