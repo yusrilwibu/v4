1f54304664fec74351c03434d1b1479025899bb0
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-20 13:20:38 +0800

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -1,29 +1,31 @@
-let fs = require('fs')
-let handler = async (m, { conn }) => {
-let teks = 'donasi'
-let dana = global.dana
-let pulsa = global.pulsa
-let gopay = global.gopay
-let numberowner = global.numberowner
-let anu = `*DONATE*
-Hai ðŸ‘‹
-Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
- Donasi Emoney•
-Indoosat: 085822347348
-
-Berapapun donasi kalian akan sangat berarti ‘
-
-Terimakasih Buat Yang Sudah Mau berdonasi🤗
-
-Selebihnya Bisa Hubungi Owner
-Contact person Owner:
-wa.me/6285822347348 (Owner)
-
-*donasi via follow ig juga boleh*`
-  conn.sendButtonImg(m.chat, fla + teks, anu, instagram, 'Back', '.help', m) 
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+››╭─〘 *Donasi* 〙
+╭╡📮: DONASI
+│┝‷✧ *Ovo:* 085828764046
+│┝‷✧ *Dana:* 085828764046
+│┝‷✧ *Gopay:* 085828764046
+│╰───···─────
+│⁺◛˖ Ingin Donasi? Chat nomor owner
+╰──────────···───
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '🧸 YouTube 🧸', url: syt}},
+    {index: 2, urlButton: {displayText: '💬 Instagram 💬', url: sig}},
+    {index: 3, urlButton: {displayText: '🌎 Official Group 🌎', url: sgc}},
+    {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
 }
-handler.help = ['donasi', 'donate']
-handler.tags = ['xp', 'info']
-handler.command = /^(donasi|donate)$/i
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
 
-module.exports = handler
+export default handler
