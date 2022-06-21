b5d2d353207973f677cae0b290c5223a14c8ff60
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-22 00:18:41 +0800

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -1,22 +1,29 @@
-let handler = async (m, { conn, isROwner, text }) => {
-    const delay = time => new Promise(res => setTimeout(res, time))
-    let getGroups = await conn.groupFetchAllParticipating()
-    let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
-    let anu = groups.map(v => v.id)
-    var pesan = m.quoted && m.quoted.text ? m.quoted.text : text
-    if(!pesan) throw 'teksnya?'
-    m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5 } detik`)
-    for (let i of anu) {
-    await delay(500)
-    conn.sendBut(i, `${pesan}`, wm, 'PEMILIK BOT', '.owner', null).catch(_ => _)
-    }
-    
-  m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text } ) => {
+ let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+
+conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+ for (let id of groups) {
+ let member = (await conn.groupMetadata(id)).participants.map(v => v.jid)
+conn.sendButton(id, '────━┅ *BROADCAST* ┅━────\n' + text, wm, thumbbc, [['OWNER 🎐', '.owner'],['DONASI ✨', '.donasi']], false, { contextInfo: {
+        externalAdReply: {
+            title: `${htjava} BROADCAST`,
+            body: titlebot,
+            description: titlebot,
+            mediaType: 2,
+          thumbnail: await(await fetch(thumb2)).buffer(),
+         mediaUrl: sig
+        }
+     }
+    })
+  }
+  m.reply('*D O N E !*')
 }
-handler.help = ['bcgcbot <teks>']
-handler.tags = ['owner']
-handler.command = /^((broadcastgc|bcgc)bot)$/i
+handler.command = ['bcgcb']
+handler.tags = ['host']
+handler.help = ['bcgcb']
 
-handler.owner = true
+handler.rowner = true
 
-module.exports = handler
+export default handler
