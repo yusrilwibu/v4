8fb1984946a01f725bb81ee79f70c322792327cb
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-21 17:23:47 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -730,14 +730,14 @@ module.exports = {
                 if (chat.welcome) {
                     let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
                     for (let user of participants) {
-                       let pp = './src/welcome.jpg'
+                       let pp = 'https://telegra.ph/file/64691c22ee84bde72b031.jpg'
                         try {
                             pp = await this.profilePictureUrl(user, 'image')
                         } catch (e) {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang 1'))
-                                this.sendButtonImg(id, pp, text, "Group Message", "Hi Beban 👋", "wkwk", null)
+                                this.sendButtonImg(id, pp, text, "©ArullOfc", "Okee", "wkwk", null)
                                 }
                     }
                 }
