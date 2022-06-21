a1ba40bf64cbaa93d9e94c1e60e884693bc2122d
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-21 11:58:54 +0800

    Create infogroup.js

---
+++ plugins/infogroup.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, participants, groupMetadata, text }) => {
+
+    const getGroupAdmins = (participants) => {
+        admins = []
+        for (let i of participants) {
+            i.isAdmin ? admins.push(i.jid) : ''
+        }
+        return admins
+    }
+
+    let pp = global.ppkosong
+    try {
+        pp = await conn.getProfilePicture(m.chat)
+    } catch (e) {
+    } finally {
+        let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, expired, descUpdate, stiker, privatelink } = global.db.data.chats[m.chat]
+        const groupAdmins = getGroupAdmins(participants)
+        let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split`@`[0]}`).join('\n')
+
+        if (text) return m.reply(msToDate(expired - new Date() * 1))
