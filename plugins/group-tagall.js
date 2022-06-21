91c91300689fc145e3da8c8253ffc5204bd7f153
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-22 02:19:22 +0800

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,10 +1,10 @@
 let handler = async(m, { conn, text, participants }) => {
-  let teks = `⋙ *Tagall Member* ⋘
+  let teks = `⋙ *T̳A̳G̳A̳L̳L̳ ̳M̳E̳M̳* ⋘
 \n *${text ? text : 'Nothing'}*\n\n`
 		      	for (let mem of participants) {
 		            teks += ` @${mem.id.split('@')[0]}\n`
 				}
-                teks += `\nArullOfc`
+                teks += `\nA͓̽r͓̽u͓̽l͓̽l͓̽O͓̽f͓̽c͓̽`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
