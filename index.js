d1fa417779189027b10333dfc2db836ddbfe1ffd
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-21 13:35:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('BOT BY\n HYZER', {
+CFonts.say('BOT BY\n ARULLOFC', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`BY HYZER OFFICIAL`, {                                            
+CFonts.say(`BY ARULLOFC`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
