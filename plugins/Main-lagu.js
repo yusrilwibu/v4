ce31601ce34655a8321d419b834162ea6e5bc10b
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-21 13:14:09 +0800

    Update Main-lagu.js

--- plugins/Main-lagu.js
+++ plugins/Main-lagu.js
@@ -14,7 +14,7 @@ hyzer = {
         }
        } 
      }
-conn.sendAudio(m.chat, 'https://file.io/1BZKHiUuKXgL', hyzer, { packname: "sticker by", author: "ArullOfc" })
+conn.sendFile(m.chat, 'https://file.io/1BZKHiUuKXgL', hyzer, { packname: "sticker by", author: "ArullOfc" })
 }
 
 handler.customPrefix = /^(Tes)$/i
