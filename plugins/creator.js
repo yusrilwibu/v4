2a621cea2df78626b3d8b201997a75ef3fafcce3
Warikrr <107493725+Warikrr@users.noreply.github.com>
2022-06-22 22:22:27 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,20 +1,25 @@
-const delay = time => new Promise(res => setTimeout(res, time))
-let handler = async(m, { conn }) => {
-	conn.p = conn.p ? conn.p : {}
-	let id = m.chat
-	ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
-	fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
-	conn.p[id] = [
-	await conn.sendContact(m.chat, numberowner, nameowner, { contexInfo: { forwardingScore: 99999, isForwarded: true } })
-	]
-	await delay(100)
-  return conn.sendMessage(m.chat, { text: `*Hallo kak* @${await m.sender.split('@')[0]}, *itu nomor ownerku,kalo ada kepentingan bisa dichat^_^*`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
-  await delay(100)
-  return delete conn.p[id]
+function handler(m) {
+const q = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": ""
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+		}
+	},
+	"participant": "0@s.whatsapp.net"
 }
 
-handler.help = ['owner']
+conn.sendContact(m.chat, '6285822347348', 'ArullOfc', m)
+m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
+}
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
+
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
